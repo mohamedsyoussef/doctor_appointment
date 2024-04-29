671d9986405c4c27334eeeadf8fa654d64192b9b
mohamedsyoussef <72705926+mohamedsyoussef@users.noreply.github.com>
2024-04-29 22:46:42 +0300

    Add const keyword to LoginBlocListener instantiation
    Adding the const keyword to the LoginBlocListener instantiation for improved
    performance and efficiency.

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -70,7 +70,7 @@ class LoginScreen extends StatelessWidget {
                     )
                   ],
                 ),
-                LoginBlocListener(),
+                const LoginBlocListener(),
               ],
             ),
           ),
